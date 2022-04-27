160167219cf7f45847d3086d8b5d928f8487a703
koujianfeng <koujianfeng@do-global.com>
2022-04-27 15:04:37 +0800

    feat(npm): +hap 联盟工具

---
+++ src/Common/js/static.js
@@ -0,0 +1,16 @@
+export const quickAppDoc = "https://developer.huawei.com/consumer/cn/doc/development/quickApp-References/quickapp-filestructure";
+export const huaweiAdVideo = "https://ss0.bdstatic.com/-0U0bnSm1A5BphGlnYG/cae-legoup-video-target/93be3d88-9fc2-4fbd-bd14-833bca731ca7.mp4";
+
+export const huaweiLocation = {
+    lat:'22.648984851989812',
+    lng:'114.0604739127374',
+    name:'message.huaweiLocationName'
+};
+
+export const huaweiLogoUrl = "https://res.vmallres.com/pimages//common/config/logo/SXppnESYv4K11DBxDFc2.png";
+
+export const socketUrl = "ws://echo.websocket.org";
+
+export const colorA = "#0a59f7";
+
+export const protocolAgreeStorage = "protocolAgreeKey";
---
+++ src/Common/js/utils.js
@@ -0,0 +1,20 @@
+export const strToDate = function (dateStr) {
+    let splitArr = dateStr.split(' ');
+    let dateArr = splitArr[0].split('-');
+    let timeArr = splitArr[1] ? splitArr[1].split(':') : [];
+    let newDate = new Date();
+    newDate.setFullYear(dateArr[0]);
+    newDate.setMonth(Number(dateArr[1]) - 1);
+    newDate.setDate(dateArr[2]);
+    newDate.setHours(timeArr[0] || 0, timeArr[1] || 0, timeArr[2] || 0, timeArr[3] || 0);
+    return newDate;
+}
+
+export const lessTenFormat = function(num){
+    if(isNaN(num) || num < 0)
+    {
+        return '';
+    }
+    let newNum = Number(num);
+    return newNum >= 10 ? newNum : `0${num}`
+}
